83e22ee9e60d71eae54665354ef895394b313164
Brian Dukes <bdukes@engagesoftware.com>
2009-07-20 16:13:31 +0000

    Fixed incorrect column name PositionDescription->JobDescription
    git-tfs-id: [http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010]$/Engage Employment;C3639

--- Source/EmploymentController.cs
+++ Source/EmploymentController.cs
@@ -1,55 +1,86 @@
-//Engage: Employment - http://www.engagesoftware.com
-//Copyright (c) 2004-2009
-//by Engage Software ( http://www.engagesoftware.com )
-
-//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
-//TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
-//THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
-//CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-//DEALINGS IN THE SOFTWARE.
-
-using System;
-using System.Collections;
-using System.Data;
-using System.Globalization;
-using DotNetNuke.Common.Utilities;
-using DotNetNuke.Entities.Modules;
-using DotNetNuke.Entities.Modules.Definitions;
-using DotNetNuke.Services.Search;
-using Engage.Dnn.Employment.Data;
+// <copyright file="EmploymentController.cs" company="Engage Software">
+// Engage: Employment
+// Copyright (c) 2004-2009
+// by Engage Software ( http://www.engagesoftware.com )
+// </copyright>
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
 
 namespace Engage.Dnn.Employment
 {
+    using System;
+    using System.Collections;
+    using System.Data;
+    using System.Globalization;
+    using Data;
+    using DotNetNuke.Common.Utilities;
+    using DotNetNuke.Entities.Modules;
+    using DotNetNuke.Entities.Modules.Definitions;
+    using DotNetNuke.Services.Search;
+
+    /// <summary>
+    /// Exposes the capabilities of this module to DNN.  Implements the method to allow searching and syndication integration.
+    /// </summary>
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses", Justification = "Called by DNN through reflection")]
     internal class EmploymentController : ISearchable
     {
+        /// <summary>
+        /// The file from which to retrieve localized text for this type
+        /// </summary>
         private const string LocalResourceFile = Utility.DesktopModuleRelativePath + "App_LocalResources/EmploymentController.resx";
 
         #region ISearchable Members
 
-        public SearchItemInfoCollection GetSearchItems(ModuleInfo ModInfo)
+        /// <summary>
+        /// Gets a collection of <see cref="SearchItemInfo"/> instances, describing the job openings that can be viewed in the given Job Details module.
+        /// </summary>
+        /// <param name="modInfo">Information about the module instance for which jobs should be returned.</param>
+        /// <returns>A collection of <see cref="SearchItemInfo"/> instances</returns>
+        public SearchItemInfoCollection GetSearchItems(ModuleInfo modInfo)
         {
             SearchItemInfoCollection searchItems = new SearchItemInfoCollection();
-            Hashtable moduleSettings = (new ModuleController()).GetTabModuleSettings(ModInfo.TabModuleID);
+            Hashtable moduleSettings = (new ModuleController()).GetTabModuleSettings(modInfo.TabModuleID);
 
-            //only index the JobDetail module definition (since most of this information is only viewable there,
-            //and because the Guid parameter on the SearchItemInfo ("jobid=" + jobid) gets put on the querystring to make it work all automagically).  BD
-            if (Engage.Dnn.Utility.GetBoolSetting(moduleSettings, Utility.EnableDnnSearchSetting, true) && (new ModuleDefinitionController()).GetModuleDefinitionByName(ModInfo.DesktopModuleID, ModuleDefinition.JobDetail.ToString()).ModuleDefID == ModInfo.ModuleDefID)
+            // only index the JobDetail module definition (since most of this information is only viewable there,
+            // and because the Guid parameter on the SearchItemInfo ("jobid=" + jobid) gets put on the querystring to make it work all automagically).  BD
+            if (Dnn.Utility.GetBoolSetting(moduleSettings, Utility.EnableDnnSearchSetting, true) && (new ModuleDefinitionController()).GetModuleDefinitionByName(modInfo.DesktopModuleID, ModuleDefinition.JobDetail.ToString()).ModuleDefID == modInfo.ModuleDefID)
             {
-                int? jobGroupId = Engage.Dnn.Utility.GetIntSetting(moduleSettings, Utility.JobGroupIdSetting);
+                int? jobGroupId = Dnn.Utility.GetIntSetting(moduleSettings, Utility.JobGroupIdSetting);
 
-                using (IDataReader jobs = DataProvider.Instance().GetJobs(jobGroupId, ModInfo.PortalID))
+                using (IDataReader jobs = DataProvider.Instance().GetJobs(jobGroupId, modInfo.PortalID))
                 {
                     while (jobs.Read())
                     {
                         if (!(bool)jobs["IsFilled"])
                         {
                             string jobId = ((int)jobs["JobId"]).ToString(CultureInfo.InvariantCulture);
-                            string searchItemTitle = string.Format(CultureInfo.CurrentCulture, Utility.GetString("JobInLocation", LocalResourceFile, ModInfo.PortalID), (string)jobs["JobTitle"], (string)jobs["LocationName"], (string)jobs["StateName"]);
-                            string searchedContent = HtmlUtils.StripWhiteSpace(HtmlUtils.Clean((string)jobs["JobTitle"] + " " + (string)jobs["PositionDescription"] + " " + (string)jobs["RequiredQualifications"] + " " + (string)jobs["DesiredQualifications"], false), true);
-                            string searchDescription = HtmlUtils.StripWhiteSpace(HtmlUtils.Clean((string)jobs["PositionDescription"], false), true);
+                            string searchDescription = HtmlUtils.StripWhiteSpace(HtmlUtils.Clean((string)jobs["JobDescription"], false), true);
+                            string searchItemTitle = string.Format(
+                                CultureInfo.CurrentCulture,
+                                Utility.GetString("JobInLocation", LocalResourceFile, modInfo.PortalID),
+                                (string)jobs["JobTitle"],
+                                (string)jobs["LocationName"],
+                                (string)jobs["StateName"]);
 
-                            searchItems.Add(new SearchItemInfo(searchItemTitle, searchDescription, (int)jobs["RevisingUser"], (DateTime)jobs["PostedDate"], ModInfo.ModuleID, jobId, searchedContent, "jobid=" + jobId));
+                            string searchedContent = HtmlUtils.StripWhiteSpace(
+                                HtmlUtils.Clean(
+                                    (string)jobs["JobTitle"] + " " + (string)jobs["JobDescription"] + " " + (string)jobs["RequiredQualifications"] + " " + (string)jobs["DesiredQualifications"],
+                                    false),
+                                true);
+
+                            searchItems.Add(
+                                new SearchItemInfo(
+                                    searchItemTitle,
+                                    searchDescription,
+                                    (int)jobs["RevisingUser"],
+                                    (DateTime)jobs["PostedDate"],
+                                    modInfo.ModuleID,
+                                    jobId,
+                                    searchedContent,
+                                    "jobid=" + jobId));
                         }
                     }
                 }
