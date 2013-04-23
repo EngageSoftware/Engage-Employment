9e769f019b1ff0c9fd2ee2a059580dc9ea05389c
Brian Dukes <bdukes@engagesoftware.com>
2013-04-23 19:50:32 +0000

    Bump version to 1.12.1
    git-tfs-force:Not in VS
    git-tfs-id: [http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010]$/Engage Employment;C22316

--- Source/AssemblyInfo.cs
+++ Source/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("3d5900ae-111a-45be-96b3-d9e4606ca793")]
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("1.12.0.*")]
-[assembly: AssemblyFileVersion("1.12.0.0")]
+[assembly: AssemblyVersion("1.12.1.*")]
+[assembly: AssemblyFileVersion("1.12.1.0")]
