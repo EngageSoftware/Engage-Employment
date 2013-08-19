51e99f457f17f1f539e5334befd9b75ad988bb89
Brian Dukes <bdukes@engagesoftware.com>
2013-08-19 17:49:21 +0000

    Documentation
    git-tfs-force:Not in VS
    git-tfs-id: [http://tfs.etg-inc.net:8080/tfs/Engage%20TFS%202010]$/Engage Employment;C23886

--- Source/admin/StatusListing.ascx.cs
+++ Source/admin/StatusListing.ascx.cs
@@ -3,7 +3,6 @@
 // Copyright (c) 2004-2013
 // by Engage Software ( http://www.engagesoftware.com )
 // </copyright>
-
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
 // TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
 // THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
@@ -21,29 +20,29 @@ namespace Engage.Dnn.Employment.Admin
     using DotNetNuke.Services.Localization;
     using DotNetNuke.UI.Utilities;
 
+    using Engage.Annotations;
+
+    /// <summary>An editable listing of the user statuses for this portal</summary>
     public partial class StatusListing : ModuleBase
     {
+        /// <summary>The maximum length of a user status' name</summary>
         private const int StatusMaxLength = 255;
 
+        /// <summary>Gets a regular expression which will not match text that is longer than the <see cref="StatusMaxLength"/>.</summary>
+        /// <value>A regular expression <see cref="string"/>.</value>
         protected static string MaxLengthValidationExpression
         {
-            get
-            {
-                return Utility.GetMaxLengthValidationExpression(StatusMaxLength);
-            }
+            get { return Utility.GetMaxLengthValidationExpression(StatusMaxLength); }
         }
 
+        /// <summary>Gets the validation text to show when a status' name is too long.</summary>
+        /// <value>The validation text.</value>
         protected string MaxLengthValidationText
         {
-            get
-            {
-                return string.Format(CultureInfo.CurrentCulture, this.Localize("StatusMaxLength"), StatusMaxLength);
-            }
+            get { return string.Format(CultureInfo.CurrentCulture, this.Localize("StatusMaxLength"), StatusMaxLength); }
         }
 
-        /// <summary>
-        /// Raises the <see cref="Control.Init"/> event.
-        /// </summary>
+        /// <summary>Raises the <see cref="Control.Init"/> event.</summary>
         /// <param name="e">An <see cref="EventArgs"/> object that contains the event data.</param>
         protected override void OnInit(EventArgs e)
         {
@@ -67,6 +66,9 @@ namespace Engage.Dnn.Employment.Admin
             base.OnInit(e);
         }
 
+        /// <summary>Gets the status ID from the given row.</summary>
+        /// <param name="row">The row control.</param>
+        /// <returns>An <see cref="int" />, or <c>null</c>.</returns>
         private static int? GetStatusId(Control row)
         {
             var hdnStatusId = (HiddenField)row.FindControl("hdnStatusId");
@@ -80,6 +82,7 @@ namespace Engage.Dnn.Employment.Admin
             return null;
         }
 
+        /// <summary>Binds the <see cref="StatusesGridView"/> to the list of user statuses.</summary>
         private void BindData()
         {
             var statuses = UserStatus.LoadStatuses(this.PortalId);
@@ -93,6 +96,9 @@ namespace Engage.Dnn.Employment.Admin
             this.rowNewHeader.Visible = !statuses.Any();
         }
 
+        /// <summary>Gets the status ID of the row with the given index.</summary>
+        /// <param name="rowIndex">Index of the row for which to find the status ID.</param>
+        /// <returns>An <see cref="int"/>, or <c>null</c>.</returns>
         private int? GetStatusId(int rowIndex)
         {
             if (this.StatusesGridView != null && this.StatusesGridView.Rows.Count > rowIndex)
@@ -103,6 +109,10 @@ namespace Engage.Dnn.Employment.Admin
             return null;
         }
 
+        /// <summary>Gets the name of the status in the row with the given index.</summary>
+        /// <param name="rowIndex">Index of the row for which to find the name of the status.</param>
+        /// <returns>The name of the status, or <c>null</c>.</returns>
+        [CanBeNull]
         private string GetStatusName(int rowIndex)
         {
             if (this.StatusesGridView != null && this.StatusesGridView.Rows.Count > rowIndex)
@@ -115,27 +125,31 @@ namespace Engage.Dnn.Employment.Admin
             return null;
         }
 
+        /// <summary>Hides and clears the panel for adding a new status.</summary>
         private void HideAndClearNewStatusPanel()
         {
             this.NewPanel.Visible = false;
             this.txtNewStatus.Text = string.Empty;
         }
 
+        /// <summary>Determines whether the <paramref name="newStatusName"/> is unique.</summary>
+        /// <param name="statusId">The ID of the status being renamed, or <c>null</c> for a new status.</param>
+        /// <param name="newStatusName">New name of the status.</param>
+        /// <returns><c>true</c> if the new status name is unique; otherwise, <c>false</c>.</returns>
         private bool IsStatusNameUnique(int? statusId, string newStatusName)
         {
             int? newStatusId = UserStatus.GetStatusId(newStatusName, this.PortalId);
             return !newStatusId.HasValue || (statusId.HasValue && newStatusId.Value == statusId.Value);
         }
 
+        /// <summary>Sets up the validator for a new status's name.</summary>
         private void SetupStatusLengthValidation()
         {
             this.regexNewUserStatus.ValidationExpression = MaxLengthValidationExpression;
             this.regexNewUserStatus.ErrorMessage = this.MaxLengthValidationText;
         }
 
-        /// <summary>
-        /// Handles the <see cref="Button.Click"/> event of the <see cref="AddButton"/> control.
-        /// </summary>
+        /// <summary>Handles the <see cref="Button.Click"/> event of the <see cref="AddButton"/> control.</summary>
         /// <param name="sender">The source of the event.</param>
         /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         private void AddButton_Click(object sender, EventArgs e)
@@ -144,9 +158,7 @@ namespace Engage.Dnn.Employment.Admin
             this.txtNewStatus.Focus();
         }
 
-        /// <summary>
-        /// Handles the <see cref="LinkButton.Click"/> event of the <see cref="BackButton"/> control.
-        /// </summary>
+        /// <summary>Handles the <see cref="LinkButton.Click"/> event of the <see cref="BackButton"/> control.</summary>
         /// <param name="sender">The source of the event.</param>
         /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         private void BackButton_Click(object sender, EventArgs e)
@@ -154,9 +166,7 @@ namespace Engage.Dnn.Employment.Admin
             this.Response.Redirect(this.EditUrl(ControlKey.ManageApplications.ToString()));
         }
 
-        /// <summary>
-        /// Handles the <see cref="Button.Click"/> event of the <see cref="CancelNewButton"/> control.
-        /// </summary>
+        /// <summary>Handles the <see cref="Button.Click"/> event of the <see cref="CancelNewButton"/> control.</summary>
         /// <param name="sender">The source of the event.</param>
         /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         private void CancelNewButton_Click(object sender, EventArgs e)
@@ -164,9 +174,7 @@ namespace Engage.Dnn.Employment.Admin
             this.HideAndClearNewStatusPanel();
         }
 
-        /// <summary>
-        /// Handles the <see cref="Button.Click"/> event of the <see cref="SaveNewButton"/> control.
-        /// </summary>
+        /// <summary>Handles the <see cref="Button.Click"/> event of the <see cref="SaveNewButton"/> control.</summary>
         /// <param name="sender">The source of the event.</param>
         /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         private void SaveNewButton_Click(object sender, EventArgs e)
@@ -186,9 +194,7 @@ namespace Engage.Dnn.Employment.Admin
             }
         }
 
-        /// <summary>
-        /// Handles the <see cref="GridView.RowCancelingEdit"/> event of the <see cref="StatusesGridView"/> control.
-        /// </summary>
+        /// <summary>Handles the <see cref="GridView.RowCancelingEdit"/> event of the <see cref="StatusesGridView"/> control.</summary>
         /// <param name="sender">The source of the event.</param>
         /// <param name="e">The <see cref="GridViewCancelEditEventArgs"/> instance containing the event data.</param>
         private void StatusesGridView_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
@@ -197,9 +203,7 @@ namespace Engage.Dnn.Employment.Admin
             this.BindData();
         }
 
-        /// <summary>
-        /// Handles the <see cref="GridView.RowCommand"/> event of the <see cref="StatusesGridView"/> control.
-        /// </summary>
+        /// <summary>Handles the <see cref="GridView.RowCommand"/> event of the <see cref="StatusesGridView"/> control. </summary>
         /// <param name="sender">The source of the event.</param>
         /// <param name="e">The <see cref="GridViewCommandEventArgs"/> instance containing the event data.</param>
         private void StatusesGridView_RowCommand(object sender, GridViewCommandEventArgs e)
@@ -231,9 +235,7 @@ namespace Engage.Dnn.Employment.Admin
             }
         }
 
-        /// <summary>
-        /// Handles the <see cref="GridView.RowDataBound"/> event of the <see cref="StatusesGridView"/> control.
-        /// </summary>
+        /// <summary>Handles the <see cref="GridView.RowDataBound"/> event of the <see cref="StatusesGridView"/> control.</summary>
         /// <param name="sender">The source of the event.</param>
         /// <param name="e">The <see cref="GridViewRowEventArgs"/> instance containing the event data.</param>
         private void StatusesGridView_RowDataBound(object sender, GridViewRowEventArgs e)
@@ -263,9 +265,7 @@ namespace Engage.Dnn.Employment.Admin
             }
         }
 
-        /// <summary>
-        /// Handles the <see cref="GridView.RowDeleting"/> event of the <see cref="StatusesGridView"/> control.
-        /// </summary>
+        /// <summary>Handles the <see cref="GridView.RowDeleting"/> event of the <see cref="StatusesGridView"/> control.</summary>
         /// <param name="sender">The source of the event.</param>
         /// <param name="e">The <see cref="GridViewDeleteEventArgs"/> instance containing the event data.</param>
         private void StatusesGridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
@@ -278,9 +278,7 @@ namespace Engage.Dnn.Employment.Admin
             }
         }
 
-        /// <summary>
-        /// Handles the <see cref="GridView.RowEditing"/> event of the <see cref="StatusesGridView"/> control.
-        /// </summary>
+        /// <summary>Handles the <see cref="GridView.RowEditing"/> event of the <see cref="StatusesGridView"/> control.</summary>
         /// <param name="sender">The source of the event.</param>
         /// <param name="e">The <see cref="GridViewEditEventArgs"/> instance containing the event data.</param>
         private void StatusesGridView_RowEditing(object sender, GridViewEditEventArgs e)
@@ -290,9 +288,7 @@ namespace Engage.Dnn.Employment.Admin
             this.BindData();
         }
 
-        /// <summary>
-        /// Handles the <see cref="Control.Load"/> event of this control.
-        /// </summary>
+        /// <summary>Handles the <see cref="Control.Load"/> event of this control.</summary>
         /// <param name="sender">The source of the event.</param>
         /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         private void Page_Load(object sender, EventArgs e)
